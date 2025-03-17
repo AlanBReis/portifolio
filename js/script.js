e0d7f582e5697c6410f684b29035396af5d33dc9
Alan dos Reis <alanzit0xz@gmail.com>
2025-03-17 13:49:44 -0300

    Att Bio, Add Cv Download, Add Sessao Atualizacoes sobre mim

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Devops.","Engenheiro de Software."],
+		strings: ["Devops.","Computação em Nuvem"],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
